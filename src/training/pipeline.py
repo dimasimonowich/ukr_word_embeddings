2b06a2208e9becf0e7b8dc48316f81c69410e613
dimasimonowich <dima.simonowich@gmail.com>
2023-06-12 01:12:40 +0300

    minor

--- src/training/pipeline.py
+++ src/training/pipeline.py
@@ -6,6 +6,9 @@ from torch.utils.data import DataLoader, random_split
 from tqdm import tqdm
 
 
+torch.manual_seed(0)
+
+
 class Pipeline:
     def __init__(self, model):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
